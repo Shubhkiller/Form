de66b33d6029899549e471610562759465b95b42
Shubham <shubhamtripathi033@gmail.com>
2023-12-17 17:11:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return ( 
+  <div>
+  <h1>Registration Form</h1>
+  <p>Please fill out this form with the required information</p>
+  <form method="post" action="https://register-demo.freecodecamp.org">
+    <fieldset>
+      <label htmlFor="first-name">Enter Your First Name: <input id="first-name" name="first-name" type="text" required /></label>
+      <label htmlFor="last-name">Enter Your Last Name: <input id="last-name" name="last-name" type="text" required /></label>
+      <label htmlFor="email">Enter Your Email: <input id="email" name="email" type="email" required /></label>
+      <label htmlFor="new-password">Create a New Password: <input id="new-password" name="new-password" type="password" pattern="[a-z0-5]{8,}" required /></label>
+    </fieldset>
+    <fieldset>
+      <legend>Account type (required)</legend>
+      <label htmlFor="personal-account"><input id="personal-account" type="radio" name="account-type" className="inline" defaultChecked /> Personal</label>
+      <label htmlFor="business-account"><input id="business-account" type="radio" name="account-type" className="inline" /> Business</label>
+    </fieldset>
+    <fieldset>
+      <label htmlFor="profile-picture">Upload a profile picture: <input id="profile-picture" type="file" name="file" /></label>
+      <label htmlFor="age">Input your age (years): <input id="age" type="number" name="age" min={13} max={120} /></label>
+      <label htmlFor="referrer">How did you hear about us?
+        <select id="referrer" name="referrer">
+          <option value>(select one)</option>
+          <option value={1}>freeCodeCamp News</option>
+          <option value={2}>freeCodeCamp YouTube Channel</option>
+          <option value={3}>freeCodeCamp Forum</option>
+          <option value={4}>Other</option>
+        </select>
+      </label>
+      <label htmlFor="bio">Provide a bio:
+        <textarea id="bio" name="bio" rows={3} cols={30} placeholder="I like coding on the beach..." defaultValue={""} />
+      </label>
+    </fieldset>
+    <label htmlFor="terms-and-conditions">
+      <input className="inline" id="terms-and-conditions" type="checkbox" required name="terms-and-conditions" /> I accept the <a href="https://www.freecodecamp.org/news/terms-of-service/">terms and conditions</a>
+    </label>
+    <input type="submit" defaultValue="Submit" />
+  </form>
+</div>
+  );
+}
+
+export default App;
